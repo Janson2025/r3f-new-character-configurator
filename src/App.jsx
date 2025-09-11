cbfb8e4b247c76e3c9105d38dacec39dcf73a42b
jason2025 <callajames2025@gmail.com>
2025-09-11 11:15:15 -0600

    changed box geometry to cylinder geometry

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, Sphere } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+
 
 function App() {
 
   return (
     <>
       <Canvas camera={{
-            position: [-4, 3, 6]
+            position: [ 3, 3, 3 ]
           }}
         >
         <color attach="background" args={['#f0f0f0']} />
         <OrbitControls />
         <mesh>
-          <boxGeometry args={[0.5, 0.5, 0.5]}/>
+          <cylinderGeometry args={[1, 1, 1, 32]}/>
           <meshNormalMaterial />
         </mesh>
       </Canvas>
